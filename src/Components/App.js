993dd0fef12d935e6621e6d0e4959b9a7ecfc412
mangomateo <velarde.matthew@gmail.com>
2021-05-05 19:09:35 -0700

    import API Key into project

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SearchForm from './SearchForm';
 import Navigation from './Navigation';
 import PhotoContainer from './PhotoContainer';
+import apiKey from '../config';
 
 class App extends Component {
 
