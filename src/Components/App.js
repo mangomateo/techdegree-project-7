4ae49ab9328e0e01fa9ba3e81fa5d95d7da00e7b
mangomateo <velarde.matthew@gmail.com>
2021-05-08 19:56:21 -0700

    request 3 initial photosets from flickr

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import axios from 'axios';
 
 // App component imports
 import SearchForm from './SearchForm';
 import Navigation from './Navigation';
 import PhotoContainer from './PhotoContainer';
 import NotFound from './NotFound';
-// import apiKey from '../config';
+import apiKey from '../config';
 
 class App extends Component {
 
   state = {
-    photoData: []
+    catPics: [],
+    dogPics: [],
+    hamsterPics: []
+  }
+
+  componentDidMount() {
+
+    // Request cat pics
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&is_getty=true&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({ catPics: response.data.photos.photo })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+
+    // Request dog pics
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=dogs&is_getty=true&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({ dogPics: response.data.photos.photo })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+
+    // Request hamster pics
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=hamster&is_getty=true&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({ hamsterPics: response.data.photos.photo })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
   }
 
   render() {
@@ -20,7 +53,7 @@ class App extends Component {
         <div className="container">
           <SearchForm />
           <Navigation />
-          <Route exact path="/" component={ PhotoContainer } />
+          <Route exact path="/" render={ () => <PhotoContainer pics={ this.state } /> } />
           <Route path="/error" component={ NotFound } />
         </div>
       </BrowserRouter>
